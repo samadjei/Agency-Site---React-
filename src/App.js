b3ce7330e8f3bc667efeb9bd95a1d3d2d486fa1a
Samuel Adjei <samueladj3i@gmail.com>
2020-10-20 23:26:05 +0100

    completed banner

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import Banner from './components/banner.js';
 import Header from './components/header.js';
 import './styles/App.scss';
 
 function App() {
+	useEffect(() => {
+		let vh = window.innerHeight * 0.01;
+		document.documentElement.style.setProperty('--vh', `${vh}px`);
+	}, []);
 	return (
 		<div className="App">
 			<Header />
+			<Banner />
 		</div>
 	);
 }
