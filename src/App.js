870e7f3e71e8969f6994822718f9d1eee1497ab4
Samuel Adjei <samueladj3i@gmail.com>
2020-10-24 20:25:52 +0100

    added routing and started navigation menu

--- src/App.js
+++ src/App.js
@@ -1,66 +1,46 @@
 import React, { useEffect } from 'react';
+import { Route } from 'react-router-dom';
 import gsap from 'gsap';
-import Cases from './components/cases.js';
-import Banner from './components/banner.js';
-import Header from './components/header.js';
 import './styles/App.scss';
-import IntroOverlay from './components/introOverlay.js';
+import Header from './components/header.js';
+
+// Pages
+import Home from './pages/home.js';
+import CaseStudies from './pages/caseStudies';
+import Approach from './pages/approach';
+import Services from './pages/services';
+import About from './pages/about';
+
+const routes = [
+	{ path: '/', name: 'Home', Component: Home },
+	{ path: '/case-studies', name: 'Case Studies', Component: CaseStudies },
+	{ path: '/appraoch', name: 'Appraoch', Component: Approach },
+	{ path: '/services', name: 'Services', Component: Services },
+	{ path: '/about-us', name: 'About Us', Component: About },
+];
 
 function App() {
 	useEffect(() => {
+		// Grab inner heihgt of window for mobile reasons when delaing with vh
 		let vh = window.innerHeight * 0.01;
 		document.documentElement.style.setProperty('--vh', `${vh}px`);
 
 		// get ride of the flash when the screen reloads
 		gsap.to('body', 0, { css: { visibility: 'visible' } });
-
-		// timeline
-		const tl = gsap.timeline();
-
-		tl.from('.line span', 1.8, {
-			y: 100,
-			ease: 'power4.out',
-			delay: 1,
-			skeyY: 7,
-			stagger: {
-				amount: 0.3,
-			},
-		})
-			.to('.overlay-top', 1.6, {
-				height: 0,
-				ease: 'expo.inOut',
-				stagger: 0.4,
-			})
-			.to('.overlay-bottom', 1.6, {
-				width: 0,
-				ease: 'expo.inOut',
-				delay: -0.8,
-				stagger: {
-					amount: 0.4,
-				},
-			})
-			// allows you to hover again
-			.to('.intro-overlay', 0, {
-				css: {
-					display: 'none',
-				},
-			})
-			.from('.case-image img', 1.6, {
-				scale: 1.4,
-				ease: 'expo.inOut',
-				delay: -2,
-				stagger: {
-					amount: 0.4,
-				},
-			});
-	}, []);
+	});
 	return (
-		<div className="App">
-			<IntroOverlay />
+		<>
+			{/* map through the routes and return the components */}
+
 			<Header />
-			<Banner />
-			<Cases />
-		</div>
+			<div className="App">
+				{routes.map(({ path, Component }) => (
+					<Route key={path} exact path={path}>
+						<Component />
+					</Route>
+				))}
+			</div>
+		</>
 	);
 }
 
