8200bdce5c08b1cca26586f7f10bbec18a81e0ad
Samuel Adjei <samueladj3i@gmail.com>
2020-10-24 22:42:08 +0100

    added window/height update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Route } from 'react-router-dom';
 import gsap from 'gsap';
 import './styles/App.scss';
+
+//Components
+import Navigation from './components/navigation';
 import Header from './components/header.js';
 
 // Pages
@@ -19,20 +22,52 @@ const routes = [
 	{ path: '/about-us', name: 'About Us', Component: About },
 ];
 
+function debounce(fn, ms) {
+	let timer;
+	return () => {
+		clearTimeout(timer);
+		timer = setTimeout(() => {
+			timer = null;
+			fn.apply(this, arguments);
+		}, ms);
+	};
+}
+
 function App() {
+	// get ride of the flash when the screen reloads
+	gsap.to('body', 0, { css: { visibility: 'visible' } });
+	// define a dimension state
+	const [dimensions, setDimensions] = useState({
+		// cpturing the width and height and setting them to the width and height properties
+		height: window.innerHeight,
+		width: window.innerWidth,
+	});
+
 	useEffect(() => {
 		// Grab inner heihgt of window for mobile reasons when delaing with vh
-		let vh = window.innerHeight * 0.01;
+		let vh = dimensions.height * 0.01;
 		document.documentElement.style.setProperty('--vh', `${vh}px`);
 
-		// get ride of the flash when the screen reloads
-		gsap.to('body', 0, { css: { visibility: 'visible' } });
+		// sets the state to the height and width
+		const debouncedHandleResize = debounce(function handleResize() {
+			setDimensions({
+				height: window.innerHeight,
+				width: window.innerWidth,
+			});
+		}, 1000);
+
+		window.addEventListener('resize', debouncedHandleResize);
+
+		// removes the event listener from the window
+		return () => {
+			window.removeEventListener('resize', debouncedHandleResize);
+		};
 	});
 	return (
 		<>
-			{/* map through the routes and return the components */}
-
 			<Header />
+			{console.log(dimensions)}
+			{/* map through the routes and return the components */}
 			<div className="App">
 				{routes.map(({ path, Component }) => (
 					<Route key={path} exact path={path}>
@@ -40,6 +75,7 @@ function App() {
 					</Route>
 				))}
 			</div>
+			<Navigation />
 		</>
 	);
 }
